e30250edfe691cba490a4ba4e07414e58a49e805
Brock Wilcox <awwaiid@thelackthereof.org>
2012-07-13 16:49:03 -0400

    Decode the world in randombot for fun

--- bot/random.pl
+++ bot/random.pl
@@ -4,6 +4,7 @@ use v5.10;
 use strict;
 use warnings;
 use IO::Scalar;
+use JSON::XS;
 
 STDIN->autoflush(1);
 STDOUT->autoflush(1);
@@ -11,7 +12,13 @@ STDOUT->autoflush(1);
 my @moves = qw( L R U D W );
 
 while(1) {
-  my $world = <>;
+  my $input = <>;
+  exit unless $input;
+  chomp $input;
+
+  # we don't use world... but if we did...
+  my $world = decode_json($input);
+
   my $move = $moves[int rand scalar @moves];
   print "$move\n";
 }
