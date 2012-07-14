e348e922fa2623165bb8740219e6dafaf230d50c
borbyu <jason@woys.org>
2012-07-14 01:30:56 +0000

    small update

--- lamda-miner/viewer.php
+++ lamda-miner/viewer.php
@@ -22,7 +22,7 @@ function generateState($state) {
     global $dead_miner;
     $state_parts = json_decode($state, true);
     $out = "";
-    foreach ($state_parts as $k=>$r) {
+    foreach ($state_parts as $k => $r) {
         if (!in_array($k, array('score','lamda_count','robot_loc','lambda_remain','ending',
         	'flooding_step', 'waterproof', 'waterproof_step'))) continue;
         if (is_array($r)) $r = json_encode($r);
