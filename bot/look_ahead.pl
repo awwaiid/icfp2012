d19091c8ea6e6fc71cc05286b55e24dafd84e93a
lungching <mburns.lungching@gmail.com>
2012-07-15 20:19:40 -0400

    7 step look ahead cut losses if 25 under top score abort if only option is to die

--- bot/look_ahead.pl
+++ bot/look_ahead.pl
@@ -19,7 +19,7 @@ STDOUT->autoflush(1);
 
 # my @types = qw( L R U D W A );
 my @types = qw( L R U D );
-my $look_ahead = 5;
+my $look_ahead = 7;
 my @moves = tuples_with_repetition( \@types, $look_ahead);
 
 local $SIG{INT} = sub {
@@ -29,6 +29,7 @@ local $SIG{INT} = sub {
 
 my $input = <>;
 
+my $top_score = 0;
 while(1) {
     exit unless $input;
     chomp $input;
@@ -75,7 +76,7 @@ while(1) {
 
         $choices->{$this_set} = $set_score;
     }
-say STDERR Dumper($choices);
+# say STDERR Dumper($choices);
 
     # $scores{ <score value> } = [<array of moves that come to that score>]
     my %scores;
@@ -86,13 +87,33 @@ say STDERR Dumper($choices);
     # my %by_score = reverse %$choices;
     my $best_score = max keys %scores;
 
-    my @next_moves = shuffle( @{ $scores{ $best_score } } );
-say STDERR "best score is $best_score : movement is " . $next_moves[0];
-    my @sequence = split('_', $next_moves[0] );
-
-    for my $move ( @sequence ) {
-        print "$move\n";
+    if ( $top_score - $best_score > 25 ) {
+        # cut our losses
+        print "A\n";
         $input = <>;
+        next;
+    }
+    elsif ( $best_score > $top_score ){
+    $top_score = $best_score;
+    }
+
+
+
+    #  we couldn't avoid dying so might as well abort
+    if ( $best_score == -1000000 ) {
+        print "A\n";
+        $input = <>;
+    }
+    else {
+
+        my @next_moves = shuffle( @{ $scores{ $best_score } } );
+# say STDERR "best score is $best_score : movement is " . $next_moves[0];
+        my @sequence = split('_', $next_moves[0] );
+
+        for my $move ( @sequence ) {
+            print "$move\n";
+            $input = <>;
+        }
     }
 }
 
